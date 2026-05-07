c6f8daa64564f122915305f7ff4c8a6c3d7a094d
tania <tnt.taniatariq@gmail.com>
2026-05-07 11:17:23 +0500

    added home route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,9 +8,12 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const upload = multer({storage: multer.memoryStorage()})
+const upload = multer({ storage: multer.memoryStorage() })
 
-app.post('/create-post',upload.single("image"),async (req,res) => {
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+app.post('/create-post', upload.single("image"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   console.log(req.image)
@@ -20,25 +23,25 @@ app.post('/create-post',upload.single("image"),async (req,res) => {
 
   const post = await postModel.create({
     image: result.url,
-    caption:req.body.caption
+    caption: req.body.caption
   })
 
   return res.status(201).json({
-    message:"Post created successfully",
-    post:post
+    message: "Post created successfully",
+    post: post
 
   })
 
- })
+})
 
- app.get('/posts',async(req,res) => {
+app.get('/posts', async (req, res) => {
 
   const posts = await postModel.find();
 
   return res.status(200).json({
-    message:"posts fetched successfully",
-    posts:posts
+    message: "posts fetched successfully",
+    posts: posts
   })
- })
+})
 
 module.exports = app;
